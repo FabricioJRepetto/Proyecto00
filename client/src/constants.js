5aa6e9bed75b0fd1f602d1695fd9cf3626eab0da
Fabricio Repetto <93217713+FabricioJRepetto@users.noreply.github.com>
2022-05-13 18:02:18 -0300

    url nueva database

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,14 +1,18 @@
-const API_DOGS = "http://localhost:3001/dogs/";
-const API_TEMPS = "http://localhost:3001/temperaments/";
-const API_NAMES = 'http://localhost:3001/dogs/names';
-const API_POST = 'http://localhost:3001/dogs/';
-const API_DEL = 'http://localhost:3001/dogs/';
+const API_DOGS = "https://doghouse-api.herokuapp.com/dogs/";
+const API_TEMPS = "https://doghouse-api.herokuapp.com/temperaments/";
+const API_NAMES = 'https://doghouse-api.herokuapp.com/dogs/names';
+const API_POST = 'https://doghouse-api.herokuapp.com/dogs/';
+const API_DEL = 'https://doghouse-api.herokuapp.com/dogs/';
 const TEMPS_PER_CARD = 3;
 
 module.exports = { API_DOGS, API_TEMPS, API_NAMES, API_POST, API_DEL, TEMPS_PER_CARD };
 
+// const API_DOGS = "http://localhost:3001/dogs/";
+// const API_TEMPS = "http://localhost:3001/temperaments/";
+// const API_NAMES = 'http://localhost:3001/dogs/names';
+// const API_POST = 'http://localhost:3001/dogs/';
+// const API_DEL = 'http://localhost:3001/dogs/';
 /*
-? get /dogs/?name=
 ? get /dogs/:id
 
 : post /dogs/{req.body}
